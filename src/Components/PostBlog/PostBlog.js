91f3f777f4ae40a28b06aa5ec9af12f7df023d16
Mosaddek Sohel <tanjimsohel>
2022-02-27 01:23:39 +0600

    fetch image

--- src/Components/PostBlog/PostBlog.js
+++ src/Components/PostBlog/PostBlog.js
@@ -7,25 +7,52 @@ const PostBlog = () => {
     const [post, setPost] = useState('');
     const [image, setImage] = useState(null);
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!image) {
+            return;
+        }
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('post', post);
+        formData.append('image', image)
+
+
+        fetch('', {
+            method: 'POST',
+            body: formData
+
+        })
+
+    }
+
+
+
+
 
     return (
         <div>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <TextField
                     label="Title"
                     sx={{ width: '35%', marginBottom: '15px' }}
                     type="text"
+                    onClick={e => setTitle(e.target.value)}
                     required
                     variant="standard" />
                 <br />
                 <TextField
                     sx={{ width: '35%', marginBottom: '5px' }}
+                    onClick={e => setPost(e.target.value)}
+
                     required
                 />
                 <br />
                 <Input
                     accept="image/*"
-                    multiple type="file" />
+                    multiple type="file"
+                    onClick={e => setImage(e.target.files[0])}
+                />
                 <br />
                 <Button variant="contained" component="span">
                     Upload
